f0f53ecf6af2f71bd174a29ec1c835d49bd11b62
Tie Teck Huong <100077541@students.swinburne.edu.my>
2019-10-22 20:53:18 +0800

    Fixing HighScoreController.cs, MenuController.cs and UtilityFunctions.cs

--- src/Model/ISeaGrid.cs
+++ src/Model/ISeaGrid.cs
@@ -38,6 +38,7 @@ public interface ISeaGrid
 	/// <param name="col">the column of the tile</param>
 	/// <returns>the result of the attack</returns>
 	AttackResult HitTile(int row, int col);
+
 }
 
 //=======================================================
