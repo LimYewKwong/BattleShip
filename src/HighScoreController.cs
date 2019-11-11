a78939c547c6fc3e516219e3b0d34db9132f2c70
LimYewKwong <55531060+LimYewKwong@users.noreply.github.com>
2019-11-11 16:21:09 +0800

    Fixed high score bug (after restart)

--- src/HighScoreController.cs
+++ src/HighScoreController.cs
@@ -210,7 +210,7 @@ static class HighScoreController
             _Scores.RemoveAt(_Scores.Count - 1);
             _Scores.Add(s);
             _Scores.Sort();
-
+			SaveScores();
             GameController.EndCurrentState();
         }
     }
